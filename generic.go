284b6f76f045edd5a05e9266aee3d458a69671d1
Tom Kay <oridan82@gmail.com>
2025-12-02 11:07:59 +0000

    add ternary `If` function (#2)
    * add Ternary function
    
    * move to generic
    
    * move ternary test
    
    * rename to If

--- generic.go
+++ generic.go
@@ -22,3 +22,11 @@ func Overlaps[T comparable](a, b []T) bool {
 	}
 	return false
 }
+
+// If A generic ternary function which returns vtrue if cond is true, otherwise it returns vfalse.
+func If[T any](cond bool, vtrue, vfalse T) T {
+	if cond {
+		return vtrue
+	}
+	return vfalse
+}
